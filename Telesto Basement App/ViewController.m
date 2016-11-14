2de3647dfa49e0802b77f7867ad00a55926c2d1a
Emon <Emon@Emons-Mac-mini.local>
2016-11-14 15:56:12 +0600

    Login Page UI (Without graphics)

--- Telesto Basement App/ViewController.m
+++ Telesto Basement App/ViewController.m
@@ -18,9 +18,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
+    [self createView];
+
 }
 
+-(void)createView{
 
+//    /*Logo*/
+//    UIImage *logoImage = [UIImage imageNamed:@"1.png"];
+//    UIImageView *logoView = [[UIImageView alloc] init];
+//    logoView.image = logoImage;
+//    logoView.frame = CGRectMake((self.view.frame.size.width-logoImage.size.width)/2, 15, logoImage.size.width, logoImage.size.height);
+//    [_baseView addSubview:logoView];
+//
+//    /*UserNameField*/
+//    UIImage *userNameImage = [UIImage imageNamed:@"1.png"];
+//    UIImageView *userNameImageView = [[UIImageView alloc] init];
+//    userNameImageView.image = userNameImage;
+//    userNameImageView.frame = CGRectMake((self.view.frame.size.width-userNameImage.size.width)/2, (self.view.frame.size.height-userNameImage.size.height)/2, userNameImage.size.width, userNameImage.size.height);
+//    [_baseView addSubview:userNameImageView];
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
