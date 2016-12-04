f96ff806dc5aa979dcad75b8ae330ad650510b6e
Evan Islam <evan@centralstationmarketing.com>
2016-12-04 19:21:31 +0600

    Customer Record page edit.

---
+++ Telesto Basement App/CustomerRecordViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomerRecordViewController.h
+//  Telesto Basement App
+//
+//  Created by CSM on 12/4/16.
+//  Copyright © 2016 csm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomerRecordViewController : UIViewController
+
+@end
---
+++ Telesto Basement App/CustomerRecordViewController.m
@@ -0,0 +1,41 @@
+//
+//  CustomerRecordViewController.m
+//  Telesto Basement App
+//
+//  Created by CSM on 12/4/16.
+//  Copyright © 2016 csm. All rights reserved.
+//
+
+#import "CustomerRecordViewController.h"
+
+@interface CustomerRecordViewController ()
+
+@end
+
+@implementation CustomerRecordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)customerRecordPage:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
