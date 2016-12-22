74fa6894f8fd5fcca53d74e3065221b02899731b
Evan Islam <evan@centralstationmarketing.com>
2016-12-22 17:20:51 +0600

    Customer List and Drawing Window

--- Telesto Basement App/CustomerListViewController.m
+++ Telesto Basement App/CustomerListViewController.m
@@ -24,6 +24,7 @@
 }
 
 -(void)getCustomerData{
+    _customerListTableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
 
     NSString *customerData = @"{ \"firstName\": \"John\",\"lastName\": \"Smith\",\"age\": 25,\"address\":\{\"streetAddress\":\"21 2nd Street\",\"city\":\"New York\",\"state\":\"NY\",        \"postalCode\":\"10021\"},\"phoneNumber\":[{\"type\": \"home\",\"number\": \"212 555-1234\"},{\"type\":\"fax\",\"number\": \"646 555-4567\"}]}";
     // Convert to JSON object:
@@ -84,7 +85,7 @@
 }
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     
-    return [_customerInfoObjArray count];
+    return 10;//[_customerInfoObjArray count];
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
 
@@ -98,47 +99,80 @@
     UILabel *userAddress;
     UILabel *scheduleLabel;
     UIImageView *userImageView;
+    UIButton *viewButton;
     
-    CustomerInfoObject *customerInfoObject = [_customerInfoObjArray objectAtIndex:indexPath.row];
+//    CustomerInfoObject *customerInfoObject = [_customerInfoObjArray objectAtIndex:indexPath.row];
 
     if (cell) {
         userImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"userName"]];
         [cell addSubview:userImageView];
-        userImageView.frame = CGRectMake(3, 2, 45, 45);
+        userImageView.frame = CGRectMake(3,150/2, 50, 50);
         userImageView.tag = 0;
         
-        userName = [[UILabel alloc] initWithFrame:CGRectMake(48, 2, 200, 50)];
+        userName = [[UILabel alloc] initWithFrame:CGRectMake(60, 2, 500, 50)];
         [cell addSubview:userName];
+        userName.font = [UIFont fontWithName:@"Roboto-Bold" size:30];
         userName.tag = 1;
         
-        userAddress = [[UILabel alloc] initWithFrame:CGRectMake(48, 52, 200, 150)];
+        userAddress = [[UILabel alloc] initWithFrame:CGRectMake(60, 52, 500, 100)];
         [cell addSubview:userAddress];
         userAddress.tag = 2;
+        userAddress.font = [UIFont fontWithName:@"Roboto-Light" size:14];
+        userAddress.numberOfLines = 10;
         
-        scheduleLabel = [[UILabel alloc] initWithFrame:CGRectMake(48, 252, 200, 48)];
+        scheduleLabel = [[UILabel alloc] initWithFrame:CGRectMake(60, 152, 500, 30)];
         [cell addSubview:scheduleLabel];
         scheduleLabel.tag = 3;
+        
+        viewButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [viewButton addTarget:self action:@selector(cellMethod:) forControlEvents:UIControlEventTouchUpInside];
+        [viewButton setTitle:@"Show View" forState:UIControlStateNormal];
+        viewButton.backgroundColor = [UIColor redColor];
+        viewButton.frame = CGRectMake(tableView.frame.size.width - 165, (184-40)/2, 160.0, 40.0);
+        viewButton.tag = 5;
+        [cell addSubview:viewButton];
     }
     else{
         userImageView = [cell viewWithTag:0];
         userName = [cell viewWithTag:1];
         userAddress = [cell viewWithTag:2];
         scheduleLabel = [cell viewWithTag:3];
+        
     }
-    NSLog(@"\nCustomer Name: %@ \n Customer Address: %@ \n Customer Schedule: %@ \n",customerInfoObject.customerName,customerInfoObject.customerAddress,customerInfoObject.scheduleDate);
-    userName.text = [NSString stringWithFormat:@"%@", customerInfoObject.customerName];
-    userAddress.text = [NSString stringWithFormat:@"%@", customerInfoObject.customerAddress];
-    scheduleLabel.text = [NSString stringWithFormat:@"%@", customerInfoObject.scheduleDate];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    userName.backgroundColor = [UIColor clearColor];
+    userAddress.backgroundColor = [UIColor clearColor];
+    scheduleLabel.backgroundColor = [UIColor clearColor];
+    userName.text = @"Steve Lenon";
+    userAddress.text = @"city : New York.\npostalCode : 10021.\nstate : NY.\nstreetAddress : 21 2nd Street.";
+    scheduleLabel.text = @"Monday, June 15, 2009 8:45:30 PM ";
+//    NSLog(@"\nCustomer Name: %@ \n Customer Address: %@ \n Customer Schedule: %@ \n",customerInfoObject.customerName,customerInfoObject.customerAddress,customerInfoObject.scheduleDate);
+//    userName.text = [NSString stringWithFormat:@"%@", customerInfoObject.customerName];
+//    userAddress.text = [NSString stringWithFormat:@"%@", customerInfoObject.customerAddress];
+//    scheduleLabel.text = [NSString stringWithFormat:@"%@", customerInfoObject.scheduleDate];
 
     return cell;
 }
+-(void)cellMethod:(id)sender{
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *vc = [sb instantiateViewControllerWithIdentifier:@"DesignViewController"];
+    vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    [self.navigationController pushViewController:vc animated:YES];
+}
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
 
-    return 200;
+    return 184;
 
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
+}
+- (IBAction)createNewCustomer:(id)sender {
+    
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *vc = [sb instantiateViewControllerWithIdentifier:@"CustomerRecordViewController"];
+    vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    [self.navigationController pushViewController:vc animated:YES];
 }
 - (IBAction)testBtnCalled:(id)sender {
     UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
