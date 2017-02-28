bfc26b65fac1e452d0a9b910728ed60717c5b238
Evan Islam <evanislam@Evans-Mac-mini.local>
2017-02-28 14:08:59 +0600

    Revert the slog.

--- Telesto Basement App/BaseViewController.m
+++ Telesto Basement App/BaseViewController.m
@@ -39,7 +39,6 @@
         [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"First_Logged_in"];
         [self displayTermsAndCondition];
     }
-    NSLog(@"Test push for subdevelopment");
 }
 -(void)viewDidLayoutSubviews{
 
