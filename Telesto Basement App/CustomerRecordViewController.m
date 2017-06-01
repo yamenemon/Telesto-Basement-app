723872ddecb8e8fecd52fa22312dae71943ed578
Evan Islam <evan@centralstationmarketing.com>
2017-06-01 20:04:37 +0600

    Change of create customer page with web service initial.

--- Telesto Basement App/CustomerRecordViewController.m
+++ Telesto Basement App/CustomerRecordViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "CustomerRecordViewController.h"
+#define BaseURL  @"http://telesto.centralstationmarketing.com/"
+
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 @interface CustomerRecordViewController ()
 
@@ -19,7 +21,16 @@
     // Do any additional setup after loading the view.
     self.title = @"Customer Records";
     
-    
+    [self changeTextfieldStyle:_firstNameTextField];
+    [self changeTextfieldStyle:_lastNameTextField];
+    [self changeTextfieldStyle:_streetAddressTextField];
+    [self changeTextfieldStyle:_cityTextField];
+    [self changeTextfieldStyle:_zipCodeTextField];
+    [self changeTextfieldStyle:_countryTextField];
+    [self changeTextfieldStyle:_emailTextField];
+    [self changeTextfieldStyle:_areaTextField];
+    [self changeTextfieldStyle:_phoneNumberTextField];
+    [self changeTextfieldStyle:_stateNameTextField];
     
 }
 -(void)viewDidLayoutSubviews{
@@ -34,16 +45,16 @@
     self.basementImageView.layer.borderColor = [UIColor lightGrayColor].CGColor;
     self.basementImageView.clipsToBounds = YES;
     
-    [self changeTextfieldStyle:_firstNameTextField];
-    [self changeTextfieldStyle:_lastNameTextField];
-    [self changeTextfieldStyle:_streetAddressTextField];
-    [self changeTextfieldStyle:_cityTextField];
-    [self changeTextfieldStyle:_zipCodeTextField];
-    [self changeTextfieldStyle:_countryTextField];
-    [self changeTextfieldStyle:_emailTextField];
-    [self changeTextfieldStyle:_areaTextField];
-    [self changeTextfieldStyle:_phoneNumberTextField];
-    [self changeTextfieldStyle:_stateNameTextField];
+//    [self changeTextfieldStyle:_firstNameTextField];
+//    [self changeTextfieldStyle:_lastNameTextField];
+//    [self changeTextfieldStyle:_streetAddressTextField];
+//    [self changeTextfieldStyle:_cityTextField];
+//    [self changeTextfieldStyle:_zipCodeTextField];
+//    [self changeTextfieldStyle:_countryTextField];
+//    [self changeTextfieldStyle:_emailTextField];
+//    [self changeTextfieldStyle:_areaTextField];
+//    [self changeTextfieldStyle:_phoneNumberTextField];
+//    [self changeTextfieldStyle:_stateNameTextField];
     
 }
 -(UITextField*)changeTextfieldStyle:(UITextField*)textField{
@@ -56,6 +67,54 @@
     tempTextField.clipsToBounds      = YES;
     return tempTextField;
     
+}
+-(void)createCustomer{
+
+//    tokenAsString = @"telesto9NRd7GR11I41Y20P0jKN146SYnzX5uMH";
+    NSString *endPoint = @"create_customer";
+//    fName, lName, address, city, state, zip, countryId, email, phone, userId
+    NSString *post = [NSString stringWithFormat:@"fName=%@&lName=%@&address=%@&city=%@&state=%@&zip=%@&countryId=%@&email=%@&phone=%@&userId=%@", _firstNameTextField.text, _lastNameTextField.text, _streetAddressTextField.text,_cityTextField.text,_stateNameTextField.text,_zipCodeTextField.text,_countryTextField.text];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%lu",(unsigned long)[postData length]];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",BaseURL,endPoint]]];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:postData];
+    [self sendRequest:request];
+
+}
+-(void)sendRequest:(NSURLRequest*)urlRequest{
+    
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: nil delegateQueue: [NSOperationQueue mainQueue]];
+    
+    
+    NSURLSessionDataTask * dataTask =[defaultSession dataTaskWithRequest:urlRequest
+                                                       completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                           NSLog(@"Response:%@ %@\n", response, error);
+                                                           
+                                                           if(error == nil)
+                                                           {
+                                                               NSString * text = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+                                                               NSLog(@"Data = %@",text);
+                                                               if (data) {
+                                                                   [self parseJOSNLoginStatus:data];
+                                                               }
+                                                               else{
+                                                                   
+                                                               }
+                                                           }
+                                                       }];
+    [dataTask resume];
+}
+- (BOOL)parseJOSNLoginStatus:(NSData *)data {
+    NSError *e = nil;
+    NSDictionary *jsonArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error: &e];
+    long loginStatus = [[jsonArray objectForKey:@"success"] longValue];
+    return YES;
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
