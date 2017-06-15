7b5dc3301076a83209fc17262093d8d2ce581183
Evan Islam <evan@centralstationmarketing.com>
2017-06-15 19:52:38 +0600

    mapit functionality.

---
+++ Telesto Basement App/MapItViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapItViewController.h
+//  Telesto Basement App
+//
+//  Created by CSM on 6/15/17.
+//  Copyright © 2017 csm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapItViewController : UIViewController
+
+@end
---
+++ Telesto Basement App/MapItViewController.m
@@ -0,0 +1,49 @@
+//
+//  MapItViewController.m
+//  Telesto Basement App
+//
+//  Created by CSM on 6/15/17.
+//  Copyright © 2017 csm. All rights reserved.
+//
+
+#import "MapItViewController.h"
+
+@interface MapItViewController ()
+
+@end
+
+@implementation MapItViewController
+
+- (void)viewDidLoad {
+    // AIzaSyB4I45UeOkUSSvnW070-1GFEf0THZjvZn0
+    [super viewDidLoad];
+//    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.868
+//                                                            longitude:151.2086
+//                                                                 zoom:6];
+//    GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+//    
+//    GMSMarker *marker = [[GMSMarker alloc] init];
+//    marker.position = camera.target;
+//    marker.snippet = @"Hello World";
+//    marker.appearAnimation = kGMSMarkerAnimationPop;
+//    marker.map = mapView;
+//    
+//    self.view = mapView;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
